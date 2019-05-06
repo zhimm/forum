0bd3dee1bd0b13c48cd1a76b8f45dee32b81ae88
unknown <syedazhim10@gmail.com>
2019-05-06 16:18:47 +0800

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <router-view class="container" />
+  </div>
+</template>
+
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+
+  components: {
+    NavBar
+  }
+  
+}
+</script>
+
---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">WELCOME TO FORUM</h1>
+      <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore vero, sequi alias voluptatibus harum recusandae at voluptate ea fugit quasi?</p>
+      <hr class="my-4">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse consequatur error iure fugit. Aut, rem?</p>
+      <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'home',
+};
+</script>
